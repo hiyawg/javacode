12773d4d733a7df26dcf8951e6d2650cb56c4408
admin <admin@admins-MacBook-Pro.local>
2019-08-03 15:58:56 -0400

    hkkk

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,8 +2,13 @@ package com.example.demo.controller;
 
 
 import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,4 +35,16 @@ public class UserController {
     void addUser(@RequestBody User user) {
         userRepository.save(user);
     }
+    
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<String> deleteCustomer(@PathVariable("id") long id) {
+      System.out.println("Delete Customer with ID = " + id + "...");
+   
+      userRepository.deleteById(id);
+   
+      return new ResponseEntity<>("Customer has been deleted!", HttpStatus.OK);
+    }
+ 
+    
+   
 }
